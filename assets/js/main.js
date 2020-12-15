b83bcac2bdb75ee967e336b07887c6726e1e47b5
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2020-12-15 15:28:38 +0100

    Prima parte

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+let app = new Vue ({
+    el: "#app",
+    data: {
+        counter : 0,
+        images: [
+            "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
+            "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
+            "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
+            "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"
+        ]
+    },
+    methods: {
+        next(){
+            this.counter++;
+            if(this.counter === this.images.length){
+                this.counter = 0;
+            }
+
+        },
+        prev(){
+            if(this.counter > 0){
+                this.counter --;
+            }else{
+                this.counter = this.images.length - 1;
+            }
+        }
+    }
+});
